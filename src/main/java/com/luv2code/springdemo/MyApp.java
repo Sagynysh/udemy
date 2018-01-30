34c2d3f36f87408990c4cb05dc609d897a47e8b3
Кенел Сағыныш Болатұлы <s.kenel@kazpost.kz>
2018-01-30 11:07:44 +0600

    ClassPathXmlApplicationContext example

--- src/main/java/com/luv2code/springdemo/MyApp.java
+++ src/main/java/com/luv2code/springdemo/MyApp.java
@@ -1,11 +1,16 @@
 package com.luv2code.springdemo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by S.KENEL on 29.01.2018.
  */
 public class MyApp {
     public static void main(String[] args) {
-        Coach theCoach = new TrackCoach();
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Coach theCoach = context.getBean("myCoach",Coach.class);
+
+//        Coach theCoach = new TrackCoach();
         System.out.println(theCoach.getDAilyWorkout());
     }
 }
